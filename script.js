b49630a7fba53d6a0f9d86856a77a761dc8b1cc1
JuanCaviedes <88120075+JuanCaviedes@users.noreply.github.com>
2021-07-28 22:37:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let inputs = document.querySelectorAll(".valor");
+
+inputs.forEach(input => {
+  input.value = input.dataset.cambio;   
+});
+
+
+function valorModificado(input){
+  let factor = input.value / input.dataset.cambio;
+
+  inputs.forEach(input => {
+    input.value = (input.dataset.cambio * factor).toFixed(2);   
+  });
+
+  
+}
